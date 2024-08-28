4f1c346089305e68841b0194386de808aa2238ba
Pushkar <pushkardayal07@gmail.com>
2024-08-28 18:20:49 +0530

    first project added

---
+++ firstpeoject/src/main/java/com/pushkar/firstpeoject/FirstpeojectApplication.java
@@ -0,0 +1,13 @@
+package com.pushkar.firstpeoject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstpeojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstpeojectApplication.class, args);
+	}
+
+}
